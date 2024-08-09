c60c623fd4bb07bc54a6c3057502a1862ad1abd8
Matthew Carmichael <sealblox@gmail.com>
2024-08-09 21:54:18 +0000

    Refreshed build files for 0.2.0

--- build/lib/sealtui/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-sealtui
-a package for easily creating cli's and tui's.
-"""
-
-__version__ = "0.1.0"
-__author__ = "Matthew Carmichael"
--- build/lib/sealtui/core.py
+++
@@ -1,9 +0,0 @@
-from sealtui import styler, styles, errors
-
-def sealprint(*args, end="\n", flush=True):
-    for txt in args:
-        styled_text = styler.StyleString(txt)
-        print(styled_text, end=end, flush=flush)
-
-def styled(arg):
-    return styler.StyleString(arg)
